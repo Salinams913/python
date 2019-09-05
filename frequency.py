8168f5b9b1a05eb2df55574b090c30a8bc357a8f
Salinams913 <53892882+Salinams913@users.noreply.github.com>
2019-09-05 12:59:54 +0530

    Create frequency.py

---
+++ frequency.py
@@ -0,0 +1,13 @@
+def char_frequency(str1):
+    dict = {}
+    for n in str1:
+        keys = dict.keys()
+        if n in keys:
+            dict[n] += 1
+        else:
+            dict[n] = 1
+    print(dict)
+
+str1=input("Enter a string:")
+char_frequency(str1)
+
